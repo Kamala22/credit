1ead66d53efcc1ecff0b4ea45137877c34c176fe
kamala <mavicmason@gmail.com>
2023-06-01 11:20:16 +0800

    1st commit

--- credit.java
+++ credit.java
@@ -2,3 +2,5 @@ credit card payment
 [master (root-commit) 80b0b3b] 1st commit
  1 file changed, 1 insertion(+)
  create mode 100644 credit.java
+[master 7167554] 1st commit
+ 1 file changed, 3 insertions(+)
